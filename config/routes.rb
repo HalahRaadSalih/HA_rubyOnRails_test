021c2b333ab45ad006efe545e9ef6476b1860202
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-10-24 18:41:55 -0700

    add get /auth/signout route to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/' => 'pages#home'
   get '/auth/signin' => 'users#index'
   post '/auth/signin' => 'users#sign_in'
+  get '/auth/signout' => 'users#sign_out'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
