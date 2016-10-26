2eeeb4a6face885a2856b20fcc601a098b9af1d2
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-10-25 18:38:54 -0700

    add new method to create new user, addd user_params to get user params + add create method to create new user with params from sign_up_form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,5 +13,20 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  private def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to '/auth/signin'
+    else
+      render 'new'
+    end
+  end
+  
 end
