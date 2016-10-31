4ab740d870c33e92b465198b7d077862d6977f08
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-10-30 22:11:27 -0700

    use open weather api to request current weather

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,17 @@
+require 'rest-client'
+require 'json'
+
 class LocationsController < ApplicationController
+  API_KEY = Rails.application.secrets.WEATHER_API_KEY
+  API_URL = 'http://api.openweathermap.org/data/2.5/weather'
+  ZIP_CODE = "98101"
+  URL = API_URL.to_s + '?zip='+ ZIP_CODE.to_s+',us&APPID='+ API_KEY.to_s
+
   def index
+    response = RestClient.get  URL.to_s
+    print JSON.parse(response.body)
+    weather = JSON.parse(response.body);
+    @current_weather = weather["weather"][0]["main"]
+    @temprature = weather["main"]["temp"]
   end
 end
